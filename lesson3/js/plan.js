443dbfadd098c40a647cc8270dd9456e5e0de414
kir15006 <kir15006@byui.edu>
2021-05-12 14:21:17 -0600

    created weather site plan

--- lesson3/js/plan.js
+++ lesson3/js/plan.js
@@ -4,6 +4,4 @@ window.addEventListener('load', () => {
 
     hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
-
-    //window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 });
