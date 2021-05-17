c22eb90f0b9123787884a9c5b7645e4894ccb067
kir15006 <kir15006@byui.edu>
2021-05-17 10:45:52 -0600

    added date to footer

--- lesson4/js/preston-4.js
+++ lesson4/js/preston-4.js
@@ -1,7 +1,16 @@
+
+var date = new Date();
+var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday']
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+
 window.addEventListener('load', () => {
     const hambutton = document.querySelector('.ham');
     const mainnav = document.querySelector('#navigation');
 
     hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+    const cry = document.querySelector("#currentDate");
+    cry.textContent = dayNames[date.getDay()] + ', ' + date.getDate() + ' ' + monthNames[date.getMonth()] + ' ' + date.getFullYear();
 
 });
