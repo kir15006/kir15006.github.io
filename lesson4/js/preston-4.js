a8e1da848aeebdebf00d3d5e0a886e8872543903
kir15006 <kir15006@byui.edu>
2021-05-17 10:49:14 -0600

    fixed date javascript

--- lesson4/js/preston-4.js
+++ lesson4/js/preston-4.js
@@ -1,14 +1,12 @@
-
-var date = new Date();
-var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday']
-var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
-
 window.addEventListener('load', () => {
     const hambutton = document.querySelector('.ham');
     const mainnav = document.querySelector('#navigation');
 
     hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+    var date = new Date();
+    var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday']
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
     const cry = document.querySelector("#currentDate");
     cry.textContent = dayNames[date.getDay()] + ', ' + date.getDate() + ' ' + monthNames[date.getMonth()] + ' ' + date.getFullYear();
